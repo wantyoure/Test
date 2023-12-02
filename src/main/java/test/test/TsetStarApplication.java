a9b7727325f40e5081e68a359e2f4786578255d6
seungko <qkrtmdgh55@naver.com>
2023-12-02 19:01:16 +0900

    test

---
+++ src/main/java/test/test/TsetStarApplication.java
@@ -0,0 +1,13 @@
+package test.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsetStarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsetStarApplication.class, args);
+	}
+
+}
---
+++ src/main/java/test/test/test/TestCode.java
@@ -0,0 +1,8 @@
+package test.test.test;
+
+public class TestCode {
+
+    public static void main(String[] args) {
+        System.out.println("TEST");
+    }
+}
