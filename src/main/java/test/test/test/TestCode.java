db5eba0670a26933bc70ddda285135ff4adc0f57
seungko <qkrtmdgh55@naver.com>
2023-12-02 19:05:12 +0900

    TEST Commit2

--- src/main/java/test/test/test/TestCode.java
+++ src/main/java/test/test/test/TestCode.java
@@ -5,6 +5,8 @@ public class TestCode {
     public static void main(String[] args) {
         System.out.println("TEST");
 
-        System.out.println("TEST Commit");
+        System.out.println("TEST Commit1");
+
+        System.out.println("TEST Commit2");
     }
 }
